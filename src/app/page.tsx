ab8beab5c1fc29b921944f548f4f242ab5f382fa
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2025-09-24 10:29:12 +0200

    fixed styles on main page, added revalidate every 5 min

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@ import { fetchFromDB } from "@/lib/helpers";
 import { Movie } from "@/types/movie";
 import MovieCard from "@/components/MovieCard";
 
+export const revalidate = 300;
+
 export default async function Home() {
   const moviesData = await fetchFromDB<{ results: Movie[] }>("/movie/popular");
   const movies = moviesData.results;
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="p-6">
-        <h1>Popular Movies</h1>
+   <main className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Popular Movies</h1>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           {movies.map((movie) => (
               <MovieCard key={movie.id} movie={movie} />
           ))}
         </div>
       </main>
-    </div>
   );
 }
