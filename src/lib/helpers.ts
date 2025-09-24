2fc0a8eb7fa631f7ead83dab736fde4be7ea0668
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2025-09-24 10:28:30 +0200

    fixed bug in query

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -4,7 +4,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 export async function fetchFromDB<T> (endpoint: string, params: Record<string, string> = {}): Promise<T> {
     const query = new URLSearchParams({ api_key: API_KEY ?? "", language: "en-US",  ...params });
     // no caching
-    const res = await fetch(`${BASE_URL}${endpoint}${query}`, { next: { revalidate: 0 } });
+    const res = await fetch(`${BASE_URL}${endpoint}?${query}`, { next: { revalidate: 0 } });
     if (!res.ok) throw new Error("Failed to fetch from DB");
     return res.json();
 };
